615aa30a621decb694c74acc59e7e2350518c37d
OpMarko <69360452+OpMarko@users.noreply.github.com>
2024-07-02 22:22:03 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,7 +11,7 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 100) {
+    if (rank > 50) {
         return 0;
     }
     if (rank > 1 && percent < 100) {
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-19.5*Math.pow(rank-1, 0.4) + 100) *
+    let score = (-20*Math.pow(rank-1, 0.4) + 100) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
